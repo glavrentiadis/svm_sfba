3a4187477b181cf42a115c8a61ff8898dc97f8aa
Greg Lavrentiadis <glavrentiadis@gmail.com>
2022-09-15 16:35:13 -0700

    Linear Regression files
      Changes:
        commit files for linear regression
         new file:   fit_indiv_vel_models_Jian.py
         modified:   ../preprocessing/prepare_flatfile.py

---
+++ Analyses/indiv_reg/fit_indiv_vel_models_Jian.py
@@ -0,0 +1,245 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 25 06:10:11 2022
+
+@author: glavrent
+"""
+
+#load variables
+import os
+import sys
+import pathlib
+#string libraries
+import re
+#arithmetic libraries
+import numpy as np
+import numpy.matlib
+from scipy import interpolate as interp
+#statistics libraries
+import pandas as pd
+#plot libraries
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+from matplotlib.ticker import  AutoLocator as plt_autotick
+import arviz as az
+mpl.use('agg')
+#stan library
+import cmdstanpy
+
+#%% Define Variables
+### ======================================
+
+z_star = 2.5
+
+#regression info
+fname_stan_model = '../stan_lib/jian_fun_indiv_reg_linear_res.stan'
+#iteration samples
+n_iter_warmup   = 5000
+n_iter_sampling = 5000
+#MCMC parameters
+n_chains        = 4
+adapt_delta     = 0.8
+max_treedepth   = 10
+
+
+# Input/Output
+# --------------------------------
+#input flatfile
+fname_flatfile = '../../Data/vel_profiles/all_velocity_profles.csv'
+# fname_flatfile = '../../Data/vel_profiles/Jian_velocity_profles.csv'
+
+#flag truncate
+flag_trunc_z1 = True
+
+#output filename
+# fname_out_main = 'all'
+# fname_out_main = 'Jian'
+fname_out_main = 'all_trunc'
+#output directory
+dir_out = '../../Data/indiv_reg/bayesian_fit/' + fname_out_main + '/'
+dir_fig = dir_out + 'figures/'
+
+#%% Load Data
+### ======================================
+#load velocity profiles
+df_velprofs = pd.read_csv(fname_flatfile)
+
+if flag_trunc_z1:
+    df_velprofs = df_velprofs.loc[~df_velprofs.flag_Z1,:]
+
+#%% Run Regression
+### ======================================
+#create output directory
+pathlib.Path(dir_out).mkdir(parents=True, exist_ok=True) 
+pathlib.Path(dir_fig).mkdir(parents=True, exist_ok=True) 
+
+#velocity profile ids
+vel_ids, vel_idx = np.unique(df_velprofs[['DSID','VelID']].values, axis=0, return_index=True)
+# vel_ids = vel_ids[:100, ]
+n_vel   = vel_ids.shape[0]
+
+#initalize paramters
+df_param_all = df_velprofs[['DSID','DSName','VelID','VelName','Vs30','Lat','Lon']].iloc[vel_idx,:].reset_index(drop=True)
+
+# Iterate over profiles
+# --------------------------------
+for k, v_id in enumerate(vel_ids):
+    print('Regressing profile %i of %i'%(k+1, n_vel))
+    #extract vel profile of interest
+    df_vel = df_velprofs.loc[np.logical_and(df_velprofs.DSID==v_id[0], df_velprofs.VelID==v_id[1]), ]
+    #fitted profile
+    name_vel =  df_vel.DSName.values[0]+' '+df_vel.VelName.values[0]
+    fname_vel = (fname_out_main + '_vel_prof_' + name_vel).replace(' ','_')
+   
+    # regression input data
+    # ---   ---   ---   ---
+    #prepare regression data
+    stan_data = {'N':      len(df_vel),
+                 'z_star': z_star,
+                 'Z':      df_vel.Depth_MPt.values,
+                 'Y':      df_vel.Vs.values,
+                }
+    #write as json file
+    fname_stan_data = dir_out + fname_vel + '_stan_data' + '.json'
+    cmdstanpy.utils.jsondump(fname_stan_data, stan_data)
+
+    # run stan
+    # ---   ---   ---   ---
+    #compile stan model
+    stan_model = cmdstanpy.CmdStanModel(stan_file=fname_stan_model) 
+    stan_model.compile(force=True)
+    #run full MCMC sampler
+    stan_fit = stan_model.sample(data=fname_stan_data, chains=n_chains, 
+                                 iter_warmup=n_iter_warmup, iter_sampling=n_iter_sampling,
+                                 seed=1, max_treedepth=max_treedepth, adapt_delta=adapt_delta,
+                                 show_progress=True,  output_dir=dir_out+'stan_fit/')
+
+    # process regression output
+    # ---   ---   ---   ---
+    #parameter names 
+    names_param= ['VS0','k','n','sigma']
+
+    #extract parameter posterior samples
+    stan_posterior = np.stack([stan_fit.stan_variable(n_p) for n_p in names_param], axis=1)
+
+    #save raw-posterior distribution
+    df_stan_posterior_raw = pd.DataFrame(stan_posterior, columns = names_param)
+    df_stan_posterior_raw.to_csv(dir_out + fname_vel + '_stan_posterior_raw' + '.csv', index=False)
+    
+    #summarize posterior distributions of hyper-parameters
+    perc_array = np.array([0.05,0.25,0.5,0.75,0.95])
+    df_stan_param = df_stan_posterior_raw[names_param].quantile(perc_array)
+    df_stan_param = df_stan_param.append(df_stan_posterior_raw[names_param].mean(axis = 0), ignore_index=True)
+    df_stan_param.index = ['prc_%.2f'%(prc) for prc in perc_array]+['mean'] 
+    df_stan_param.to_csv(dir_out + fname_vel + '_stan_parameters' + '.csv', index=True)
+    
+    #mean parameters
+    param_vs0 = df_stan_param.loc['mean','VS0']
+    param_k   = df_stan_param.loc['mean','k']
+    param_n   = df_stan_param.loc['mean','n']
+    param_sig = df_stan_param.loc['mean','sigma']
+    df_param_all.loc[k,['VS0','k','n','sigma']] = [param_vs0,param_k,param_n,param_sig]
+    
+    #mean model and residuals
+    vs_model = param_vs0 * np.maximum(1, (1 + param_k*(df_vel.Depth.values-z_star))**param_n )
+    df_vel.loc[df_vel.index,'Vs_model'] = vs_model 
+    df_vel.loc[df_vel.index,'res']      = df_vel.Vs.values - vs_model 
+    df_velprofs.loc[df_vel.index,'Vs_model'] = vs_model 
+    df_velprofs.loc[df_vel.index,'res']      = df_vel.Vs.values - vs_model 
+
+    # figures
+    # ---   ---   ---   ---
+    #create figure   
+    fig, ax = plt.subplots(figsize = (10,10))
+    hl1 = ax.plot(df_vel.Vs,       df_vel.Depth, linewidth=2.0, label='Original profile')
+    hl2 = ax.plot(df_vel.Vs_model, df_vel.Depth, linewidth=2.0, label='Velocity model')
+    #edit properties
+    ax.invert_yaxis()
+    ax.set_xlabel('$V_S$ (m/sec)',  fontsize=30)
+    ax.set_ylabel('Depth (m)',      fontsize=30)
+    ax.grid(which='both')
+    ax.legend(loc='upper right', fontsize=30)
+    ax.tick_params(axis='x', labelsize=25)
+    ax.tick_params(axis='y', labelsize=25)
+    ax.xaxis.set_tick_params(which='major', size=10, width=2, direction='in', top='on')
+    ax.yaxis.set_tick_params(which='major', size=10, width=2, direction='in', right='on')
+    ax.set_title(name_vel, fontsize=22)
+    fig.tight_layout()
+    fig.savefig( dir_fig + fname_vel + '.png' )
+
+#delete json files
+fname_dir = np.array( os.listdir(dir_out) )
+#velocity filenames
+fname_json = fname_dir[ [bool(re.search('\.json$',f_d)) for f_d in fname_dir] ]
+for f_j in fname_json: os.remove(dir_out + f_j)
+
+# Summary
+# --------------------------------
+# output
+# ---   ---   ---   ---
+#regression model and residuals
+df_velprofs.to_csv(dir_out + fname_out_main + '_all' + '_stan_residuals' + '.csv', index=True)
+#regression parameters
+df_param_all.to_csv(dir_out + fname_out_main + '_all' + '_stan_parameters' + '.csv', index=True)
+
+print('Standard Dev: %.1f'%df_velprofs.res.std())
+
+# figures
+# ---   ---   ---   ---
+# cmpare model parameters versus vs30
+#Vs0
+fname_fig = (fname_out_main + '_param_' + 'param_Vs0').replace(' ','_')
+fig, ax = plt.subplots(figsize = (10,10))
+hl1 = ax.plot(df_param_all.Vs30, df_param_all.VS0, 'o', label='Linear residuals')
+#edit properties
+ax.set_xlabel('$V_{S30}$ (m/sec)',  fontsize=30)
+ax.set_ylabel('$V_{S0}$ (m/sec)',  fontsize=30)
+ax.grid(which='both')
+ax.legend(loc='upper right', fontsize=30)
+ax.tick_params(axis='x', labelsize=25)
+ax.tick_params(axis='y', labelsize=25)
+fig.tight_layout()
+fig.savefig( dir_fig + fname_fig + '.png' )
+
+#k
+fname_fig = (fname_out_main + '_param_' + 'param_k').replace(' ','_')
+fig, ax = plt.subplots(figsize = (10,10))
+hl1 = ax.plot(df_param_all.Vs30, df_param_all.k, 'o', label='Linear residuals')
+#edit properties
+ax.set_xlabel('$V_{S30}$ (m/sec)',  fontsize=30)
+ax.set_ylabel('$k$',  fontsize=30)
+ax.grid(which='both')
+ax.legend(loc='upper right', fontsize=30)
+ax.tick_params(axis='x', labelsize=25)
+ax.tick_params(axis='y', labelsize=25)
+fig.tight_layout()
+fig.savefig( dir_fig + fname_fig + '.png' )
+
+#n
+fname_fig = (fname_out_main + '_param_' + 'param_n').replace(' ','_')
+fig, ax = plt.subplots(figsize = (10,10))
+hl1 = ax.plot(df_param_all.Vs30, df_param_all.n, 'o', label='Linear residuals')
+#edit properties
+ax.set_xlabel('$V_{S30}$ (m/sec)',  fontsize=30)
+ax.set_ylabel('$n$',  fontsize=30)
+ax.grid(which='both')
+ax.legend(loc='upper right', fontsize=30)
+ax.tick_params(axis='x', labelsize=25)
+ax.tick_params(axis='y', labelsize=25)
+fig.tight_layout()
+fig.savefig( dir_fig + fname_fig + '.png' )
+
+#sigma
+fname_fig = (fname_out_main + '_param_' + 'param_sig').replace(' ','_')
+fig, ax = plt.subplots(figsize = (10,10))
+hl1 = ax.plot(df_param_all.Vs30, df_param_all.sigma, 'o', label='Linear residuals')
+#edit properties
+ax.set_xlabel('$V_{S30}$ (m/sec)',  fontsize=30)
+ax.set_ylabel('$\sigma$',  fontsize=30)
+ax.grid(which='both')
+ax.legend(loc='upper right', fontsize=30)
+ax.tick_params(axis='x', labelsize=25)
+ax.tick_params(axis='y', labelsize=25)
+fig.tight_layout()
+fig.savefig( dir_fig + fname_fig + '.png' )
--- Analyses/preprocessing/prepare_flatfile.py
+++ Analyses/preprocessing/prepare_flatfile.py
@@ -35,29 +35,41 @@ def CalcVS30(depth, vel):
     
     return vs30
 
-#read Jian velocity profiles
-def ReadJianProfs(dir_name, fname_coor=None, dsid=np.nan, dsname='N/A'):
+#read general velocity profiles
+def ReadGenProfs(dir_name, fname_vprof_info, flag_header = True, dsid=np.nan, filter_vname='(.*)', dsname='N/A'):
     
-    #directory filenames
-    fname_dir = np.array( os.listdir(dir_name) )
-    #velocity filenames
-    fname_vel = fname_dir[ [bool(re.search('\.txt$',f_d)) for f_d in fname_dir] ]
-    n_vel = len(fname_vel)
+    #read vel profile info
+    df_vprof_info = pd.read_excel(fname_vprof_info)
+    n_vel = len(df_vprof_info )
     
     #velocity profile names
-    p = re.compile('profile_(.*)\.txt')
-    name_vel = [p.findall(f_v)[0] for f_v in fname_vel]
-    
-    #read coordinates
-    df_coor = pd.read_csv(fname_coor) if not fname_coor is None else None 
-    
-    #read and merge velocity profiles
-    df_vel_profs = []
-    for k, (f_v, n_v) in enumerate(zip(fname_vel, name_vel)):
-        print(r'reading prof  %i of %i'%(k,n_vel))
+    p = re.compile(filter_vname)
+
+    df_v_profs = list()
+    for k, (_, df_vp_info) in enumerate(df_vprof_info.iterrows()):
+        print(r'reading prof  %i of %i (%s)'%(k,n_vel,df_vp_info.dataset))
         
+        #file name
+        f_v = df_vp_info.dataset
+
         #read vel profs
-        df_v = pd.read_csv(dir_name + f_v)
+        df_v = pd.read_csv(dir_name+f_v) if flag_header else pd.read_csv(dir_name+f_v, names=('Depth','Vs'))
+
+        #velocity profile name
+        n_v = p.findall(f_v)[0] 
+        if type(n_v) is tuple:  n_v = " ".join(n_v)
+
+        #top of layer depth
+        z  = df_v.Depth.values
+        #Vs values
+        vs = df_v.Vs.values
+        #skip profiles with nan
+        if np.any(np.isnan(vs)):
+            print(f'\tSkipping prof %s ...'%(df_vp_info.dataset))
+            continue
+        #assert top layer depth reported
+        assert(np.abs(z[0])<1e-9),'Error. Depths to top of layers should be reported.'
+
         #add prof id and name
         df_v.loc[:,'VelID']   = k
         df_v.loc[:,'VelName'] = n_v
@@ -65,186 +77,48 @@ def ReadJianProfs(dir_name, fname_coor=None, dsid=np.nan, dsname='N/A'):
         df_v.loc[:,'DSID']   = dsid
         df_v.loc[:,'DSName'] = dsname
         
+        #flag Z1.0
+        df_v.loc[:,'flag_Z1'] = np.cumsum( vs >= 1000 ) >= 1
+        
+        #compute layer thickness and mid-point depth
+        df_v.loc[:,'Thk'] = np.append(np.diff(z), 0)
+        #midpoint depth
+        z_mp = np.convolve(z, np.ones(2), "valid")/2
+        df_v.loc[:,'Depth_MPt'] = np.append(z_mp, 0)
+        
         #compute vs30 
         df_v.loc[:,'Vs30'] = CalcVS30(df_v.Depth.values, df_v.Vs.values)
         
-        #compute 
-        if not df_coor is None:
-            break
-        else:
-            df_v.loc[:,['lat','lon']] = np.full(2,np.nan)
+        #copy coordinates 
+        df_v.loc[:,['Lat','Lon']] = df_vp_info[['lat','long']].values
         
         #reorder columns
-        df = df_v[['DSID','DSName','VelID','VelName','Vs30','lat','lon','Depth','Vs']]
+        df_v = df_v[['DSID','DSName','VelID','VelName','Vs30','Lat','Lon','Depth','Depth_MPt','Thk','Vs','flag_Z1']]
 
         #merge dataframes
-        df_vel_profs.append(df)
+        df_v_profs.append(df_v)
         
-    return pd.concat(df_vel_profs, axis=0).reset_index(drop=True)
-
-#read Boore velocity profiles
-def ReadBooreProfs(dir_name, fname_coor=None, dsid=np.nan, dsname='N/A'):
-
-    #directory filenames
-    fname_dir = np.array( os.listdir(dir_name) )
-    #velocity filenames
-    fname_vel = fname_dir[ [bool(re.search('\.txt$',f_d)) for f_d in fname_dir] ]
-    n_vel = len(fname_vel)
-    
-    #velocity profile names
-    p = re.compile('(.*)\.txt')
-    name_vel = [p.findall(f_v)[0] for f_v in fname_vel]
-    
-    #read coordinates
-    df_coor = pd.read_csv(fname_coor) if not fname_coor is None else None 
-    
-    #read and merge velocity profiles
-    df_vel_profs = []
-    for k, (f_v, n_v) in enumerate(zip(fname_vel, name_vel)):
-        print(r'reading prof  %i of %i'%(k,n_vel))
-        
-        #read vel profs
-        df_v = pd.read_csv(dir_name + f_v, names=['Depth','Vs'])
-        #add prof id and name
-        df_v.loc[:,'VelID']   = k
-        df_v.loc[:,'VelName'] = n_v
-        #add dataset id and name
-        df_v.loc[:,'DSID']   = dsid
-        df_v.loc[:,'DSName'] = dsname
-
-        #compute vs30 
-        df_v.loc[:,'Vs30'] = CalcVS30(df_v.Depth.values, df_v.Vs.values)
-        
-        #compute 
-        if not df_coor is None:
-            break
-        else:
-            df_v.loc[:,['lat','lon']] = np.full(2,np.nan)
-        
-        #reorder columns
-        df = df_v[['DSID','DSName','VelID','VelName','Vs30','lat','lon','Depth','Vs']]
-          
-        #merge dataframes
-        df_vel_profs.append(df)
-
-    return pd.concat(df_vel_profs, axis=0).reset_index(drop=True)
-
-#read USGS velocity profiles
-def ReadUSGSProfs(dir_name, fname_coor=None, dsid=np.nan, dsname='N/A'):
-
-    #directory filenames
-    fname_dir = np.array( os.listdir(dir_name) )
-    #velocity filenames
-    fname_vel = fname_dir[ [bool(re.search('\.csv$',f_d)) for f_d in fname_dir] ]
-    n_vel = len(fname_vel)
-    
-    #velocity profile names
-    p = re.compile('(.*)_vel_profile.csv')
-    name_vel = [p.findall(f_v)[0] for f_v in fname_vel]
-    
-    #read coordinates
-    df_coor = pd.read_csv(fname_coor) if not fname_coor is None else None 
-    
-    #read and merge velocity profiles
-    df_vel_profs = []
-    for k, (f_v, n_v) in enumerate(zip(fname_vel, name_vel)):
-        print(r'reading prof  %i of %i (%s)'%(k,n_vel,f_v))
-        
-        #read vel profs
-        df_v = pd.read_csv(dir_name + f_v + '/profile.txt', names=['Depth','Vs'])
-        #add prof id and name
-        df_v.loc[:,'VelID']   = k
-        df_v.loc[:,'VelName'] = n_v
-        #add dataset id and name
-        df_v.loc[:,'DSID']   = dsid
-        df_v.loc[:,'DSName'] = dsname
-
-        #compute vs30 
-        df_v.loc[:,'Vs30'] = CalcVS30(df_v.Depth.values, df_v.Vs.values)
-        
-        #compute 
-        if not df_coor is None:
-            break
-        else:
-            df_v.loc[:,['lat','lon']] = np.full(2,np.nan)
-        
-        #reorder columns
-        df = df_v[['DSID','DSName','VelID','VelName','Vs30','lat','lon','Depth','Vs']]
-          
-        #merge dataframes
-        df_vel_profs.append(df)
-
-    return pd.concat(df_vel_profs, axis=0).reset_index(drop=True)
-
-#read VSPDB velocity profiles
-def ReadVSPDBProfs(dir_name, fname_coor=None, dsid=np.nan, dsname='N/A'):
-
-    #directory filenames
-    fname_dir = np.array( os.listdir(dir_name) )
-    #velocity filenames
-    fname_vel = fname_dir[ [bool(re.search('\.txt$',f_d)) for f_d in fname_dir] ]
-    n_vel = len(fname_vel)
-    
-    #velocity profile names
-    p = re.compile('(.*)_velocityProfile.*.txt')
-    name_vel = [p.findall(f_v)[0] for f_v in fname_vel]
-    
-    #read coordinates
-    df_coor = pd.read_csv(fname_coor) if not fname_coor is None else None 
-    
-    #read and merge velocity profiles
-    df_vel_profs = []
-    for k, (f_v, n_v) in enumerate(zip(fname_vel, name_vel)):
-        print(r'reading prof  %i of %i'%(k,n_vel))
-        
-        #read vel profs
-        df_v = pd.read_csv(dir_name + f_v)
-        #add prof id and name
-        df_v.loc[:,'VelID']   = k
-        df_v.loc[:,'VelName'] = n_v
-        #add dataset id and name
-        df_v.loc[:,'DSID']   = dsid
-        df_v.loc[:,'DSName'] = dsname
-
-        #compute vs30 
-        df_v.loc[:,'Vs30'] = CalcVS30(df_v.Depth.values, df_v.Vs.values)
-        
-        #compute 
-        if not df_coor is None:
-            break
-        else:
-            df_v.loc[:,['lat','lon']] = np.full(2,np.nan)
-        
-        #reorder columns
-        df = df_v[['DSID','DSName','VelID','VelName','Vs30','lat','lon','Depth','Vs']]
-          
-        #merge dataframes
-        df_vel_profs.append(df)
-
-    return pd.concat(df_vel_profs, axis=0).reset_index(drop=True)
+    return pd.concat(df_v_profs, axis=0).reset_index(drop=True)
 
 
 #%% Define Variables
 ### ======================================
 
 #Jian profiles
-dir_velprofs_Jian     = '../Raw_files/Datasets/csv_Jian/'
-fname_coor_Jian       = None
+dir_velprofs_Jian     = '../../Raw_files/Datasets_20220915/Jian/dataset/'
+fname_vprof_info_Jian = '../../Raw_files/Datasets_20220915/Jian/Jian.xlsx'
+filter_vname_Jian     = 'profile_(.*)\.txt'
 #Boore profiles
-dir_velprofs_Boore    = '../Raw_files/Datasets/Boore/'
-fname_coor_Boore      = None
-#USGS profiles (stations)
-dir_velprofs_USGSsta  = '../Raw_files/Datasets/USGS/Stations/'
-fname_coor_USGSsta    = None
-#USGS profiles (mesh)
-dir_velprofs_USGSmesh = '../Raw_files/Datasets/USGS/Mesh/'
-fname_coor_USGSmesh   = None
+dir_velprofs_Boore     = '../../Raw_files/Datasets_20220915/Boore/dataset/'
+fname_vprof_info_Boore = '../../Raw_files/Datasets_20220915/Boore/Boore.xlsx'
+filter_vname_Boore     = '(.*)\.txt'
 #VSPDB_Vs_Profiles
-dir_velprofs_VSPDB    = '../Raw_files/Datasets/VSPDB_Vs_Profiles/Vs/txt/'
-fname_coor_VSPDB      = None
+dir_velprofs_VSPDB     = '../../Raw_files/Datasets_20220915/VSPDB/dataset/'
+fname_vprof_info_VSPDB = '../../Raw_files/Datasets_20220915/VSPDB/VSPDB.xlsx'
+filter_vname_VSPDB     = '(.*)_velocityProfile_(.*)\.txt'
 
 #output directory
-dir_out = '../Data/vel_profiles/'
+dir_out = '../../Data/vel_profiles/'
 
 #%% Load Data
 ### ======================================
@@ -252,20 +126,16 @@ df_vel_profs = []
 
 #read Jian profiles
 print("Reading Jian's profiles")
-df_vel_profs.append( ReadJianProfs(dir_name=dir_velprofs_Jian,     fname_coor=fname_coor_Jian,     dsid=1, dsname='Jian') )
+df_vel_profs.append( ReadGenProfs(dir_name=dir_velprofs_Jian,  fname_vprof_info=fname_vprof_info_Jian,
+                                  dsid=1, dsname='Jian', filter_vname=filter_vname_Jian) )
 #read Boore profiles
 print("Reading Boore's profiles")
-df_vel_profs.append( ReadBooreProfs(dir_name=dir_velprofs_Boore,   fname_coor=fname_coor_Boore,    dsid=2, dsname='Boore') )
-#read USGS profiles (stations)
-print("Reading USGS profiles (stations)")
-df_vel_profs.append( ReadUSGSProfs(dir_name=dir_velprofs_USGSsta,  fname_coor=fname_coor_USGSsta,  dsid=3, dsname='USGS-stations') )
-#read USGS profiles (mesh)
-print("Reading USGS profiles (mesh)")
-df_vel_profs.append( ReadUSGSProfs(dir_name=dir_velprofs_USGSmesh, fname_coor=fname_coor_USGSmesh, dsid=4, dsname='USGS-mesh') )
+df_vel_profs.append( ReadGenProfs(dir_name=dir_velprofs_Boore, fname_vprof_info=fname_vprof_info_Boore,
+                                  dsid=2, dsname='Boore', filter_vname=filter_vname_Boore, flag_header=False) )
 #read VSPDB profiles
 print("Reading VSPDB profiles")
-df_vel_profs.append( ReadVSPDBProfs(dir_name=dir_velprofs_VSPDB,   fname_coor=fname_coor_VSPDB,    dsid=5, dsname='VSPDB') )
-
+df_vel_profs.append( ReadGenProfs(dir_name=dir_velprofs_VSPDB, fname_vprof_info=fname_vprof_info_VSPDB, 
+                                  dsid=3, dsname='VSPDB', filter_vname=filter_vname_VSPDB) )
 
 #concatinate all data
 df_vel_profs = pd.concat(df_vel_profs, axis=0).reset_index(drop=True)
@@ -280,12 +150,8 @@ df_vel_profs.to_csv( dir_out+'all_velocity_profles.csv', index=False )
 df_vel_profs.loc[df_vel_profs.DSID==1,].to_csv( dir_out+ 'Jian_velocity_profles.csv',     index=False )
 #save Boore profiles
 df_vel_profs.loc[df_vel_profs.DSID==2,].to_csv( dir_out+ 'Boore_velocity_profles.csv',    index=False )
-#save USGSsta profiles
-df_vel_profs.loc[df_vel_profs.DSID==3,].to_csv( dir_out+ 'USGSsta_velocity_profles.csv',  index=False )
-#save USGSmesh profiles
-df_vel_profs.loc[df_vel_profs.DSID==4,].to_csv( dir_out+ 'USGSmesh_velocity_profles.csv', index=False )
 #save VSPDB profiles
-df_vel_profs.loc[df_vel_profs.DSID==5,].to_csv( dir_out+ 'VSPDB_velocity_profles.csv',    index=False )
+df_vel_profs.loc[df_vel_profs.DSID==3,].to_csv( dir_out+ 'VSPDB_velocity_profles.csv',    index=False )
 
 
 
